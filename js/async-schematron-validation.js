766ea0b838e4751fe7073b65c32c2392b99d4628
martin-honnen <martin-honnen@users.noreply.github.com>
2024-10-14 10:13:44 +0200

    Create async-schematron-validation.js

---
+++ js/async-schematron-validation.js
@@ -0,0 +1,23 @@
+import { asyncRun } from "./schematron-validatation-worker-creation.js";
+
+async function schematronValidation(xml, schematron, dirHandle, baseUri) {
+    try {
+        setDocument(resultEditor, 'Performing Schematron validation...', 'text');
+
+        const { results, error } = await asyncRun({xml, schematrong, dirHandle, baseUri});
+
+        if (results) {
+            setDocument(resultEditor, results, 'text');
+            console.log("pyodideWorker return results: ", results);
+        } else if (error) {
+            setDocument(resultEditor, error, 'text');
+            console.log("pyodideWorker error: ", error);
+        }
+    } catch (e) {
+        console.log(
+            `Error in pyodideWorker at ${e.filename}, Line: ${e.lineno}, ${e.message}`,
+        );
+    }
+}
+
+export { schematronValidation };
