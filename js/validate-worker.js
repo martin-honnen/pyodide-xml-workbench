9b046f0f7a99271e32d15232eb710182c5d937c2
martin-honnen <martin-honnen@users.noreply.github.com>
2024-10-20 13:44:54 +0200

    Update validate-worker.js

--- js/validate-worker.js
+++ js/validate-worker.js
@@ -3,7 +3,7 @@
 // Setup your project to serve `py-worker.js`. You should also serve
 // `pyodide.js`, and all its associated `.asm.js`, `.json`,
 // and `.wasm` files as well:
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.26.2/full/pyodide.js");
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.26.3/full/pyodide.js");
 
 const python = `
 import js
